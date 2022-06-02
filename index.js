ae40e096624f693863a4ae6b9cfd91e1dab48f7b
AneteCoding <77099964+AneteCoding@users.noreply.github.com>
2022-06-02 17:10:30 +0200

    fix bug

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 let deckId
 let computerScore = 0
 let myScore = 0
-const newDeck = document.getElementById("new-deck")
 const cardsContainer = document.getElementById("cards")
-const drawCards = document.getElementById("draw-cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
 const header = document.getElementById("header")
 const remainingText = document.getElementById("remaining")
 const computerScoreEl = document.getElementById("computer-score")
@@ -14,42 +13,47 @@ function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            remainingText.textContent = `Remaining cards ${data.remaining}`
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
+            console.log(deckId)
         })
 }
 
-newDeck.addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-drawCards.addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
-                <img src=${data.cards[0].image} class = "card"/>
+                <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
-                <img src=${data.cards[1].image} class = "card"/>
+                <img src=${data.cards[1].image} class="card" />
             `
-            const winnerText = determineWinner(data.cards[0], data.cards[1])
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
             header.textContent = winnerText
 
             if (data.remaining === 0) {
-                drawCards.disabled = true
+                drawCardBtn.disabled = true
                 if (computerScore > myScore) {
+                    // display "The computer won the game!"
                     header.textContent = "The computer won the game!"
                 } else if (myScore > computerScore) {
+                    // display "You won the game!"
                     header.textContent = "You won the game!"
                 } else {
+                    // display "It's a tie game!"
                     header.textContent = "It's a tie game!"
                 }
             }
         })
 })
 
-function determineWinner(card1, card2) {
-    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-        "JACK", "QUEEN", "KING", "ACE"]
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "JACK", "QUEEN", "KING", "ACE"]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
 
